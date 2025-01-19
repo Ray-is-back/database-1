8fdbee62cf35625237e721f5af8b96cabbda359b
rill927 <kontakjbril@gmail.com>
2025-01-19 15:04:51 +0700

    Update numm.js

--- numm.js
+++ numm.js
@@ -1,3 +1,9 @@
-[
-  "6283849625415@s.whatsapp.net"
-]
+{
+  "secretCode": "RILLYZYDEV",
+  "description": "PREMIUM KEY", 
+  "lastUpdated": "2025-01-019",
+  "author": "RILLYZY", 
+  "status": "Active",
+  "license": "MIT",
+  "version": "1.0.0"
+}
