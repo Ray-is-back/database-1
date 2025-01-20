9e66068b8239b16e36f9c8e981d3d0ac5d505965
rill927 <kontakjbril@gmail.com>
2025-01-20 13:14:50 +0700

    Update numm.js

--- numm.js
+++ numm.js
@@ -3,7 +3,7 @@
   "description": "PREMIUM KEY", 
   "lastUpdated": "2025-01-019",
   "author": "RILLYZY", 
-  "status": "Active",
+  "status": "disable",
   "license": "MIT",
   "version": "1.0.0"
 }
